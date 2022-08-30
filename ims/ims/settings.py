ba6072d319a8f916a5b2c79ec08bbedf12bb9962
usama <usamak981@gmail.com>
2022-08-30 23:10:42 +0500

    init commit for branch

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ims.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ims',
+        'USER': 'root',
+        'PASSWORD': 'Root@125',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
